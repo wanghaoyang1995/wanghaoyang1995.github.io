72333c2abb77aa819ab24fd5b507e61325290049
wanghaoyang1995 <767105779@qq.com>
2024-12-09 18:32:19 +0800

    分享链接

--- hugo_site/assets/js/custom.js
+++ hugo_site/assets/js/custom.js
@@ -26,6 +26,25 @@ layui.use(function(){
       }).catch(function(error) {
         console.error('copy link error:', error);
       });
+    },
+    "btn_share_weibo": function() {
+      shareSinaWeiBo(document.title, window.location.href, null);
+    },
+    "btn_share_qq": function() {
+      shareQQ(document.title, window.location.href, null);
+    },
+    "btn_share_weixin": function() {
+      layer.open({
+        type: 1,
+        area: "auto", // 宽高
+        title: false, // 不显示标题栏
+        closeBtn: 0,
+        shadeClose: true, // 点击遮罩关闭层
+        content: $("#qrcode"),
+      });
+    },
+    "go_back_to_previous_page": function() {
+      window.history.back();
     }
   });
 });
