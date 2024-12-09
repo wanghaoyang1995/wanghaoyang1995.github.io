882c7adaadd6605d8fbaa1db437ecd89021156c3
wanghaoyang1995 <767105779@qq.com>
2024-12-09 10:53:05 +0800

    小屏导航栏显示优化

--- hugo_site/assets/js/custom.js
+++ hugo_site/assets/js/custom.js
@@ -1,1 +1,23 @@
-console.log("custom.js");
+layui.use(function(){
+  let layer = layui.layer;
+  let util = layui.util;
+  let $ = layui.$;
+
+  let element = layui.element;
+  element.render("nav");
+
+  util.on("lay-on", {
+    "collapsible_menu_show": function(){
+      layer.open({
+        type: 1,
+        offset: "l",
+        anim: "slideRight", // 从左往右
+        area: "auto",
+        shade: 0.1,
+        shadeClose: true,
+        //id: "nav_side",
+        content: $("#nav_side"),
+      });
+    },
+  });
+});
