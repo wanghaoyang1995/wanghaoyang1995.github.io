1db9af47e6b2b5f72af22e820dc6a8acebdd0fa2
wanghaoyang1995 <767105779@qq.com>
2024-12-23 22:34:10 +0800

    new project vue_app

---
+++ vue_app/src/main.ts
@@ -0,0 +1,9 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import App from './App.vue'
+
+const app = createApp(App)
+app.use(ElementPlus, { size: 'small', zIndex: 3000 })
+app.mount('#app')
