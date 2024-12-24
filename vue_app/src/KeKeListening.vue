9ed8ade2aa59d46ad035ef69b09e0020162cebe3
wanghaoyang1995 <767105779@qq.com>
2024-12-24 16:07:25 +0800

    mp3 control

--- vue_app/src/KeKeListening.vue
+++ vue_app/src/KeKeListening.vue
@@ -1,10 +1,44 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { onBeforeUnmount, onMounted, reactive, ref } from 'vue'
 import { ElConfigProvider } from 'element-plus'
 import Mp3Control from './components/Mp3Control.vue'
 
-const zIndex = ref<number>(3000)
-const size = ref<'small'>('small')
+const zIndex = ref<number>(3000);
+const size = ref<'small'>('small');
+const activeTab = ref<string>("compare_panel");
+const left_time = ref(Date.now() + 1000 * 60 * 60 * 1)
+
+const article_title = ref<string>("微软正在扩展其人工智能工具产品");
+const mp3_url = ref<string>("https://k7.kekenet.com/Sound/2024/12/voatech2412183_3806493Fp4.mp3");
+const mark_list = ref<number[]>([5.0, 10.0, 14.0, 20.0]);
+
+const input_text_form = reactive({
+  input_text: ""
+});
+
+const keke_id_form = reactive({
+  id: ""
+});
+
+const on_submit = () => {
+  console.log('submit: ' + input_text_form.input_text)
+}
+
+const on_query_article = () => {
+  console.log("onQueryArticle: " + keke_id_form.id)
+}
+
+const on_sentence_changed = (sentence_index: number) => {
+  console.log("on_sentence_changed: " + sentence_index.toString())
+}
+
+onMounted(() => {
+
+});
+
+onBeforeUnmount(() => {
+
+});
 
 </script>
 
@@ -13,8 +47,44 @@ const size = ref<'small'>('small')
     <el-row class="main_container">
       <el-col :xs="1" :md="4"></el-col>
       <el-col :xs="23" :md="16">
-        <Mp3Control />
+        <el-row>
+          <el-col :xs="18" :md="18">
+            <el-form :inline="true" :model="keke_id_form" size="default">
+              <el-form-item label="文章ID">
+                <el-input v-model="keke_id_form.id" placeholder="698455" />
+              </el-form-item>
+              <el-form-item>
+                <el-button type="primary" @click="on_query_article">获取</el-button>
+              </el-form-item>
+            </el-form>
+          </el-col>
+          <el-col :xs="6" :md="6">
+            <el-countdown
+              title="剩余计时"
+              format="HH:mm:ss"
+              :value="left_time"
+            />
+          </el-col>
+        </el-row>
+        <Mp3Control :title="article_title" :mp3_url="mp3_url" :mark_list="mark_list" @current-sentence-changed="on_sentence_changed" />
+        <el-tabs v-model="activeTab" class="tab_container">
+          <el-tab-pane label="听写批改" name="compare_panel">
+            <el-form :model="input_text_form" label-width="auto">
+              <el-form-item>
+                <el-input :autosize="{ minRows: 20, maxRows: 20 }" v-model="input_text_form.input_text" maxlength="8000" class="textarea_input" placeholder="请输入文本，评分时将忽略标点符号与空白符，大小写敏感。" show-word-limit type="textarea" />
+              </el-form-item>
+              <el-form-item>
+                <el-button type="primary" size="default" @click="on_submit">提交</el-button>
+              </el-form-item>
+            </el-form>
+          </el-tab-pane>
+          <el-tab-pane label="译文参考" name="source_text">
 
+          </el-tab-pane>
+          <el-tab-pane label="答案对照" name="diff_panel">
+
+          </el-tab-pane>
+        </el-tabs>
       </el-col>
     </el-row>
   </el-config-provider>
@@ -23,5 +93,12 @@ const size = ref<'small'>('small')
 <style scoped>
 .main_container {
   margin-top: 20px;
+}
+.tab_container {
+  margin-top: 20px;
+}
+.textarea_input {
+  font-size: large;
+  font-weight: bold;
 }
 </style>
--- vue_app/src/components/Mp3Control.vue
+++ vue_app/src/components/Mp3Control.vue
@@ -2,7 +2,7 @@
   <div class="main_container">
     <el-row class="title" :gutter="20">
       <el-col :xs="14" :md="20">
-        <el-text class="mx-1 title" size="large">标题行</el-text>
+        <el-text class="mx-1 title" size="large" tag="b">{{ props.title }}</el-text>
       </el-col>
       <el-col :xs="6" :md="2" :offset="1">
         <el-popover
@@ -10,29 +10,28 @@
           title="快捷按键"
           :width="200"
           trigger="hover"
-          content="左：上一句；
-                   右：下一句；
-                   Ctrl+左：后退10秒；
-                   Ctrl+右：前进10秒；
-                   上：增加播放速度；
-                   下：降低播放速度；
-                   空格：播放/暂停；
-                   X：开启/关闭断句播放；
-                   C：开启/关闭单句循环。"
         >
           <template #reference>
             <el-button class="m-2" text><el-text tag="b">快捷键说明</el-text></el-button>
-
           </template>
+            <pre>Left: 上一句
+Right: 下一句
+Ctrl+Left: 后退10秒
+Ctrl+Right: 前进10秒
+Up: 增加播放速度
+Down: 降低播放速度
+Space: 播放/暂停
+X: 开启/关闭断句播放
+C: 开启/关闭单句循环</pre>
         </el-popover>
       </el-col>
     </el-row>
     <el-row :gutter="20" class="time_bar">
-      <el-col :xs="6" :md="2">00:00</el-col>
+      <el-col :xs="6" :md="2">{{ formattedTime(current_time) }}</el-col>
       <el-col :xs="12" :md="20">
-        <el-slider v-model="value" :marks="marks" />
+        <el-slider v-model="current_time" @input="seek" :format-tooltip="formattedTime" :min="0" :max="audio_duration" :marks="marks" />
       </el-col>
-      <el-col :xs="6" :md="2">04:00</el-col>
+      <el-col :xs="6" :md="2">{{ formattedTime(audio_duration) }}</el-col>
     </el-row>
     <el-row :gutter="20" class="control_bar">
       <el-col :span="5" style="margin-left: 10px;">
@@ -41,76 +40,313 @@
           size="large"
           inline-prompt
           style="--el-switch-on-color: #339966; --el-switch-off-color: #336699"
-          active-text="断句"
-          inactive-text="连续"
+          active-text="单句暂停"
+          inactive-text="连续播放"
         />
       </el-col>
       <el-col :xs="0" :md="4"></el-col>
       <el-col :span="2">
-        <el-button size="large" style="font-size: 28px;" :icon="CaretLeft" circle text />
+        <el-tooltip
+          effect="dark"
+          content="上一句"
+          placement="bottom-start"
+        >
+          <el-button size="large" @click="seekLastSentence" style="font-size: 28px;" :icon="CaretLeft" circle text />
+        </el-tooltip>
       </el-col>
       <el-col :span="2">
-        <el-button size="large" style="font-size: 28px;" :icon="VideoPlay" circle text />
+        <el-tooltip
+          effect="dark"
+          content="播放/暂停"
+          placement="bottom-start"
+        >
+          <el-button size="large" @click="togglePlay" style="font-size: 28px;" :icon="playing ? VideoPause : VideoPlay" circle text />
+        </el-tooltip>
       </el-col>
       <el-col :span="2">
-        <el-button size="large" style="font-size: 28px;" :icon="CaretRight" circle text />
+        <el-tooltip
+          effect="dark"
+          content="下一句"
+          placement="bottom-start"
+        >
+          <el-button size="large" @click="seekNextSentence" style="font-size: 28px;" :icon="CaretRight" circle text />
+        </el-tooltip>
       </el-col>
       <el-col :xs="1" :md="4"></el-col>
       <el-col :span="2">
-        <el-button size="large" style="font-size: 28px;" :icon="RefreshLeft" circle text />
+        <el-tooltip
+          effect="dark"
+          content="单句循环"
+          placement="bottom-start"
+        >
+          <el-button size="large" @click="toggleSentenceLoop" :style="{ color: sentence_loop ? '#54D038' : null, fontSize: '28px' }" :icon="RefreshLeft" circle text />
+        </el-tooltip>
       </el-col>
       <el-col :xs="1" :md="0"></el-col>
       <el-col :span="2">
-        <el-select
-          v-model="audio_speed"
-          placeholder="Select"
-          style="width: 65px;"
+        <el-tooltip
+          effect="dark"
+          content="播放速度"
+          placement="bottom-start"
         >
-          <el-option label="0.5x" value="0.5" />
-          <el-option label="0.8x" value="0.8" />
-          <el-option label="1.0x" value="1.0" />
-          <el-option label="1.2x" value="1.2" />
-          <el-option label="1.5x" value="1.5" />
-          <el-option label="2.0x" value="2.0" />
-        </el-select>
+          <el-select
+            v-model="audio_speed"
+            placeholder="Select"
+            style="width: 85px;"
+            @change="audio_speed_change"
+          >
+            <el-option v-for="(item, index) in audio_speed_options"
+              :key="item"
+              :label="item + 'x'"
+              :value="item"
+            />
+          </el-select>
+        </el-tooltip>
       </el-col>
     </el-row>
   </div>
 </template>
 
 <script setup lang="ts">
+import { onBeforeUnmount, onMounted, reactive, ref, watch } from 'vue'
+import type { PropType } from 'vue'
+import { CaretRight, VideoPlay, VideoPause, CaretLeft, RefreshLeft } from '@element-plus/icons-vue'
+
 const props = defineProps({
-  mp3_url: String,
-  marks: String
-})
+  title: {
+    type: String,
+    required: true,
+    default: "<无标题>"
+  },
+  mp3_url: {
+    type: String,
+    required: true,
+    default: ""
+  },
+  mark_list: {
+    type: Array as PropType<number[]>,
+    required: true,
+    default: () => []
+  }
+});
+const emit = defineEmits<{
+  (e: 'currentSentenceChanged', current_sentence_index: number): void
+}>();
 
-import { reactive, ref } from 'vue'
-import type { CSSProperties } from 'vue'
-import { CaretRight, VideoPlay, CaretLeft, RefreshLeft } from '@element-plus/icons-vue'
+type Marks = Record<number, string>;
+const marks = props.mark_list.reduce((acc:Marks, num:number) => {
+  acc[num] = "";
+  return acc;
+}, {});
+const current_sentence_index = ref<number>(0);
+const search_sentence_index = (time: number) => {
+  if (time <= props.mark_list[0]) {
+    return 0;
+  }
+  if (time >= props.mark_list[props.mark_list.length - 1]) {
+    return props.mark_list.length - 1;
+  }
 
-interface Mark {
-  style: CSSProperties
-  label: string
+  let left = 0;
+  let right = props.mark_list.length - 2;
+  let mid = 0;
+  while (left <= right) {
+    mid = Math.floor((left + right) / 2);
+    if (props.mark_list[mid] <= time && props.mark_list[mid+1] > time) {
+      return mid;
+    }
+
+    if (props.mark_list[mid] < time) {
+      left = mid + 1;
+    } else {  // if (props.mark_list[mid] > time)
+      right = mid - 1;
+    }
+  }
+  console.error("BUG! search_sentence_index");
+  return -1;
+};
+
+const audio_speed_options = ["0.3", "0.5", "0.8", "1.0", "1.2", "1.5", "2.0", "3.0"];
+let audio_speed_index = 3;
+
+const audio_duration = ref<number>(0);
+const current_time = ref<number>(0);
+const stop_at_sentence = ref<boolean>(false);
+const playing = ref<boolean>(false);
+const sentence_loop = ref<boolean>(false);
+const audio_speed = ref<string>(audio_speed_options[audio_speed_index]);
+
+const audio : HTMLAudioElement = new Audio(props.mp3_url);
+audio.addEventListener("loadedmetadata", () => {
+  audio_duration.value = audio.duration;
+});
+audio.addEventListener("timeupdate", () => {
+  current_time.value = audio.currentTime;
+  let index = search_sentence_index(current_time.value);
+  if (index === current_sentence_index.value) {
+    return;
+  }
+
+  if (sentence_loop.value) {
+    current_time.value = props.mark_list[current_sentence_index.value];
+    audio.currentTime = current_time.value;
+  } else {
+    current_sentence_index.value = index;
+  }
+
+  if (stop_at_sentence.value) {
+    playing.value = true;
+    togglePlay();
+  }
+
+  emit("currentSentenceChanged", current_sentence_index.value);
+});
+audio.addEventListener("ended", () => {
+  playing.value = false;
+});
+
+const togglePlay = () => {
+  if (audio) {
+    if (playing.value) {
+      audio.pause();
+    } else {
+      audio.play();
+    }
+    playing.value = !playing.value;
+  }
+};
+
+const toggleStopAtSentence = () => {
+  stop_at_sentence.value = !stop_at_sentence.value;
+};
+
+const toggleSentenceLoop = () => {
+  sentence_loop.value = !sentence_loop.value;
+};
+
+const formattedTime = (seconds: number | undefined): string => {
+  if (seconds === undefined) return "00:00";
+  const mins = Math.floor(seconds / 60);
+  const secs = Math.floor(seconds % 60);
+  return `${mins.toString().padStart(2, "0")}:${secs.toString().padStart(2, "0")}`;
+};
+
+const seek = () => {
+  if (audio) {
+    let index = search_sentence_index(current_time.value);
+    current_sentence_index.value = index;
+    audio.currentTime = current_time.value;
+    emit("currentSentenceChanged", current_sentence_index.value);
+  }
+};
+
+const seekForward = () => {
+  current_time.value += 10;
+  seek();
+};
+
+const seekBackward = () => {
+  current_time.value -= 10;
+  seek();
+};
+
+const seekNextSentence = () => {
+  current_sentence_index.value += 1;
+  if (current_sentence_index.value >= props.mark_list.length) {
+    current_sentence_index.value = props.mark_list.length - 1;
+  }
+  current_time.value = props.mark_list[current_sentence_index.value];
+  seek();
+};
+
+const seekLastSentence = () => {
+  current_sentence_index.value -= 1;
+  if (current_sentence_index.value < 0) {
+    current_sentence_index.value = 0;
+  }
+  current_time.value = props.mark_list[current_sentence_index.value];
+  seek();
+};
+
+const audio_speed_change = () => {
+  if (audio) {
+    audio.playbackRate = parseFloat(audio_speed.value);
+  }
+};
+
+const audio_speed_up = () => {
+  if (audio) {
+    audio_speed_index += 1;
+    if (audio_speed_index >= audio_speed_options.length) {
+      audio_speed_index = audio_speed_options.length - 1;
+    }
+    audio_speed.value = audio_speed_options[audio_speed_index];
+    audio.playbackRate = parseFloat(audio_speed.value);
+  }
 }
 
-type Marks = Record<number, Mark | string>
+const audio_speed_down = () => {
+  if (audio) {
+    audio_speed_index -= 1;
+    if (audio_speed_index <= 0) {
+      audio_speed_index = 0;
+    }
+    audio_speed.value = audio_speed_options[audio_speed_index];
+    audio.playbackRate = parseFloat(audio_speed.value);
+  }
+}
 
-const value = ref([30, 60])
-const marks = reactive<Marks>({
-  0: '0°C',
-  8: '8°C',
-  37: '',
-  50: {
-    style: {
-      color: '#1989FA',
-    },
-    label: '50%',
-  },
-})
+const keydown_handler = (e: KeyboardEvent) => {
+  if (e.target !== document.body && !(e.target instanceof HTMLBodyElement)) return;
+  if (e.ctrlKey) {
+    switch (e.key) {
+      case "ArrowLeft":
+        seekBackward();
+        break;
+      case "ArrowRight":
+        seekForward();
+        break;
+      default:
+        break;
+    }
+  } else {
+    switch (e.key) {
+      case "ArrowLeft":
+        seekLastSentence();
+        break;
+      case "ArrowRight":
+        seekNextSentence();
+        break;
+      case "ArrowUp":
+        audio_speed_up();
+        break;
+      case "ArrowDown":
+        audio_speed_down();
+        break;
+      case " ":
+        togglePlay();
+        break;
+      case "x":
+      case "X":
+        toggleStopAtSentence();
+        break;
+      case "c":
+      case "C":
+        toggleSentenceLoop();
+        break;
+      default:
+        break;
+    }
+  }
+};
 
-const stop_at_sentence = ref<boolean>(false);
-const audio_speed = ref<string>("1.0");
+onMounted(() => {
+  document.addEventListener("keydown", keydown_handler, true);
+});
 
+onBeforeUnmount(() => {
+  document.removeEventListener("keydown", keydown_handler);
+});
 </script>
 
 <style scoped>
@@ -120,7 +356,7 @@ const audio_speed = ref<string>("1.0");
 }
 .time_bar {
   margin-top: 10px;
-  margin-bottom: 36px;
+  margin-bottom: 26px;
 }
 .control_bar {
   margin-top: 10px;
